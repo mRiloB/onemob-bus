7fb539563609647078c0d95a51a5e508b6a952cf
Murilo Benício <m.benicio@outlook.com>
2023-12-19 11:51:17 -0300

    fix: maybe now image...

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,36 +1,25 @@
+<script lang="ts" setup>
+import { version } from '../../package.json';
+</script>
+
 <template>
   <q-layout view="hHh lpR fFf">
     <q-header elevated class="bg-primary text-white">
       <q-toolbar>
         <q-toolbar-title class="flex align-items">
-          <img src="src/assets/logo-horizontal.png" height="60" />
+          <img src="../assets/logo-horizontal.png" height="60" />
         </q-toolbar-title>
 
         <!-- <q-btn dense flat icon="dashboard" @click="toggleMenu" /> -->
-        <span>v1.0.0</span>
+        <span>v{{ version }}</span>
       </q-toolbar>
     </q-header>
-
-    <q-dialog v-model="dMenu" persistent>
-      <LayoutMenu />
-    </q-dialog>
 
     <q-page-container class="app-content">
       <router-view />
     </q-page-container>
   </q-layout>
 </template>
-
-<script lang="ts" setup>
-import { ref } from 'vue';
-import LayoutMenu from 'src/components/LayoutMenu.vue';
-
-const dMenu = ref(false);
-
-function toggleMenu () {
-  dMenu.value = !dMenu.value;
-}
-</script>
 
 <style lang="scss" scoped>
 .app-content {
